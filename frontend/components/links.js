a1dcdeffd7dbcb721c8501514f2b1becad73a631
straightupjac <jdc.jaclyn@gmail.com>
2022-01-30 12:17:56 +0800

    fix link spacing

--- frontend/components/links.js
+++ frontend/components/links.js
@@ -1,4 +1,4 @@
-import { Box, Button, Stack, Typography } from "@mui/material";
+import { Button, Stack, Typography } from "@mui/material";
 import { useRouter } from 'next/router'
 
 export default function Links() {
@@ -10,30 +10,30 @@ export default function Links() {
 
   return (
     <Stack direction={{ xs: "column", sm: "column", md: "row" }} spacing={4} width={'100%'}>
-      <Box width="100%">
+      <Stack justifyContent="space-between" width="100%" height="120px">
         <Typography variant="h4" textAlign='center'>
           Subscribe to the Calendar
         </Typography>
         <Button sx={{ width: '100%' }} variant="contained" size="large">
           Events
         </Button>
-      </Box>
-      <Box width="100%" height="100%">
+      </Stack>
+      <Stack justifyContent="space-between" width="100%" height="120px">
         <Typography variant="h4" textAlign='center'>
           Contribute!
         </Typography>
         <Button sx={{ width: '100%' }} variant="contained" size="large" onClick={submitEvent}>
           Submit an event
         </Button>
-      </Box>
-      <Box width="100%" height="100%">
+      </Stack>
+      <Stack justifyContent="space-between" width="100%" height="120px">
         <Typography variant="h4" textAlign='center'>
           Subscribe to {`Contrary's`} mailing list
         </Typography>
         <Button sx={{ width: '100%' }} variant="contained" size="large">
           Stay in the loop!
         </Button>
-      </Box>
-    </Stack>
+      </Stack>
+    </Stack >
   )
 }
